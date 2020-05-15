0c95df4d43b32f735bfa13f16f4f20afebee45a9
Stefan Dieterle <stefand@artcom.de>
2020-05-15 12:34:20 +0200

    Use apt_preferences instead of apt-mark hold

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Pin apt package version
-Ansible role to pin the version of an apt package using apt-mark hold.
+Ansible role to pin the version of an apt package using apt preferences.
 
 ## Role Variables
 Available variables are listed below, along with default values `(see defaults/main.yml)`:
 ```yaml
-pin_apt_packages_version_packages: []
+package_name: null
+package_version: null
 ```
+This will create the file `/etc/apt/preferences.d/<package_name>.pref` with content:
+```
+Explanation: Pin added by Ansible role "pin-apt-package-version"
+Package: <package_name>
+Pin: version <package_version>
+Pin-Priority: 600
+```
+`package_version` should be set according to the [apt_preferences man page](http://manpages.ubuntu.com/manpages/bionic/man5/apt_preferences.5.html) and can use wildcards.
 
 ## Test
 ### Requirements
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,4 +8,43 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_python3_version_pinned(host):
-    assert host.check_output('apt-mark showholds python3') == 'python3'
+    versions = {
+        "bionic": "3.6.7-1~18.04",
+        "focal": "3.8.2-0ubuntu2",
+        "xenial": "3.5.1-3",
+        "buster": "3.7.3-1"
+    }
+    host_release = host.ansible(
+        "setup"
+    )["ansible_facts"]["ansible_distribution_release"]
+    file_content = [
+        'Explanation: Pin added by Ansible role "pin-apt-package-version"',
+        'Package: python3',
+        'Pin: version {}'.format(versions[host_release]),
+        'Pin-Priority: 600',
+        ''
+    ]
+    pref_file = host.file('/etc/apt/preferences.d/python3.pref')
+
+    assert pref_file.exists
+    assert pref_file.is_file
+    assert pref_file.user == 'root'
+    assert pref_file.group == 'root'
+    assert oct(pref_file.mode) == '0o640'
+    assert pref_file.content_string == '\n'.join(file_content)
+
+
+def test_package_policy(host):
+    versions = {
+        "bionic": "3.6.7-1~18.04",
+        "focal": "3.8.2-0ubuntu2",
+        "xenial": "3.5.1-3",
+        "buster": "3.7.3-1"
+    }
+    host_release = host.ansible(
+        "setup"
+    )["ansible_facts"]["ansible_distribution_release"]
+
+    assert host.check_output(
+        'apt-cache policy python3 | grep 600'
+    ) == ' *** {} 600'.format(versions[host_release])
